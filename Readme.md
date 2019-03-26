1963767a4f4cc2bf575a8d3bd1faad37eef5136b
Mrpaker <spectator_w@126.com>
2019-03-26 22:30:54 +0800

    submit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Readme
+
+### 启动
+
+* 全局安装 mocha
+
+```
+npm install mocha -g
+```
+
+* 启动测试
+
+```
+mocha
+```
+
+### 参考
+
+* [NodeJS Assert API](http://nodejs.cn/api/assert.html)
+* https://mochajs.org/
+
---
+++ test/test.js
@@ -0,0 +1,40 @@
+var assert = require('assert')
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1, 2, 3].indexOf(4))
+    })
+  })
+})
+
+describe('assert', function () {
+  it('a和b应当深度相等', function () {
+    var a = {
+      c: {
+        e: 1
+      }
+    }
+    var b = {
+      c: {
+        e: 1
+      }
+    }
+    assert.deepStrictEqual(a, b)
+  })
+
+  it('可以捕获并验证函数fn的错误', function () {
+    function fn() {
+      xxx;
+    }
+    assert.throws(
+      fn,
+      function(err) {
+        if (err instanceof ReferenceError) {
+          return true
+        }
+      }
+    )
+    
+  })
+})
